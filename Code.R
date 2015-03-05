0d634c174d2ab0c802e467586cd23ccd37955a1d
statsbylopez <mlopez1@skidmore.edu>
2015-03-05 17:25:52 -0500

    Update Code.R

--- Code.R
+++ Code.R
@@ -1,9 +1,15 @@
-library(MASS)
+
+
+library(stringr)
 library(sm)
+library(ggplot2)
+require("extrafont")
+require(scales)
+library(MASS)
 library(gplots)
 library(XML)
 library(stringr)
-
+require(grid)
 
 lengths<-c(27,27,29,31,31,31,31,31,33,33,33)
 
@@ -27,35 +33,12 @@ for (i in 1:length(urls)){
 
 ###START HERE
 
-
-library(stringr)
-library(sm)
-library(ggplot2)
-require("extrafont")
-require(scales)
-library(MASS)
-library(gplots)
-library(XML)
-library(stringr)
-require(grid)
-
-
 names(ncaa)[11]<-"QBR"
 ncaa1<-ncaa[ncaa$RK!="RK",]
 ncaa1$name<-str_split_fixed(as.character(ncaa1$PLAYER), ",",2)[,1]
 ncaa1$QBR<-as.numeric(as.character(ncaa1$QBR))
 ncaa1<-ncaa1[ncaa1$name!="PLAYER",]
 
-ncaa1$Outcome<-substr(ncaa1$RESULT,1,1)=="W"
-ncaa1$Decile<-with(ncaa1, cut(QBR, breaks=quantile(QBR, probs=seq(0,1, by=0.1)), 
-                              include.lowest=TRUE))
-temp<-"@"
-ncaa1$Road<-grepl(temp,ncaa1$RESULT)
-ncaa1$Location<-0
-ncaa1[ncaa1$Road=="TRUE",]$Location<-"Away"
-ncaa1[ncaa1$Road=="FALSE",]$Location<-"Home"
-careerstarts<-aggregate(QBR~name,data=ncaa1,FUN="length")
-careerstarts<-careerstarts[careerstarts$QBR>=20,]  #Minimum number of starts
 
 #Put whatever players to want in here
 
